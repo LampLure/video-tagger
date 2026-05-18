658beb3dd090ae937824e2b3df99a6b775fe4bfa
LampLure <134139200+LampLure@users.noreply.github.com>
2026-05-18 12:00:18 +0800

    Prevent overlapping audio playback by owning and stopping the current sink

--- src/audio.rs
+++ src/audio.rs
@@ -1,15 +1,15 @@
 use std::fs;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::{self, Receiver};
-use std::sync::Arc;
 use std::thread;
 
 pub struct AudioPlayer {
     play_rx: Option<Receiver<PathBuf>>,
     is_playing: bool,
-    playing_flag: Option<Arc<AtomicBool>>,
+    current_sink: Option<rodio::Sink>,
+    current_stream: Option<rodio::OutputStream>,
+    current_clip: Option<PathBuf>,
 }
 
 impl AudioPlayer {
@@ -17,7 +17,9 @@ impl AudioPlayer {
         Self {
             play_rx: None,
             is_playing: false,
-            playing_flag: None,
+            current_sink: None,
+            current_stream: None,
+            current_clip: None,
         }
     }
 
@@ -55,41 +57,55 @@ impl AudioPlayer {
                     return;
                 }
 
-                let playing = Arc::new(AtomicBool::new(true));
-                let flag = playing.clone();
-                self.playing_flag = Some(playing);
-                let clip = path;
-
-                thread::spawn(move || {
-                    if let Ok(file) = fs::File::open(&clip) {
-                        let reader = BufReader::new(file);
-                        if let Ok((_stream, handle)) = rodio::OutputStream::try_default() {
-                            if let Ok(sink) = rodio::Sink::try_new(&handle) {
-                                if let Ok(source) = rodio::Decoder::new(reader) {
-                                    sink.append(source);
-                                    sink.sleep_until_end();
-                                }
-                            }
-                        }
+                match Self::start_sink(&path) {
+                    Ok((stream, sink)) => {
+                        self.current_clip = Some(path);
+                        self.current_stream = Some(stream);
+                        self.current_sink = Some(sink);
+                        self.is_playing = true;
                     }
-                    let _ = fs::remove_file(&clip);
-                    flag.store(false, Ordering::SeqCst);
-                });
+                    Err(_) => {
+                        let _ = fs::remove_file(&path);
+                        self.is_playing = false;
+                    }
+                }
             }
         }
 
-        if let Some(ref flag) = self.playing_flag {
-            if !flag.load(Ordering::SeqCst) {
-                self.is_playing = false;
-                self.playing_flag = None;
-            }
+        if self.current_sink.as_ref().map(|sink| sink.empty()).unwrap_or(false) {
+            self.cleanup_finished_clip();
+            self.is_playing = false;
+        }
+    }
+
+    fn start_sink(path: &Path) -> Result<(rodio::OutputStream, rodio::Sink), String> {
+        let file = fs::File::open(path).map_err(|e| e.to_string())?;
+        let reader = BufReader::new(file);
+        let (stream, handle) = rodio::OutputStream::try_default().map_err(|e| e.to_string())?;
+        let sink = rodio::Sink::try_new(&handle).map_err(|e| e.to_string())?;
+        let source = rodio::Decoder::new(reader).map_err(|e| e.to_string())?;
+        sink.append(source);
+        Ok((stream, sink))
+    }
+
+    fn cleanup_finished_clip(&mut self) {
+        self.current_sink.take();
+        self.current_stream.take();
+        if let Some(path) = self.current_clip.take() {
+            let _ = fs::remove_file(path);
         }
     }
 
     pub fn stop(&mut self) {
         self.play_rx = None;
+        if let Some(sink) = self.current_sink.take() {
+            sink.stop();
+        }
+        self.current_stream.take();
+        if let Some(path) = self.current_clip.take() {
+            let _ = fs::remove_file(path);
+        }
         self.is_playing = false;
-        self.playing_flag = None;
     }
 
     pub fn is_playing(&self) -> bool {
